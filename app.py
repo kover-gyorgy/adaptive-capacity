ddcb0929173db4a53f9647c0634011381c058528
Kover Gyorgy <kover.gyorgy@ke.hu>
2020-09-01 19:07:09 +0100

    error testing on heroku

--- app.py
+++ app.py
@@ -1748,29 +1748,31 @@ def display_table(Ewe):
 
 
     paramCor_ID = paramCor[paramCor["ID"]==int(Ewe)]
+    
+    cached = paramCor_ID.iloc[0]
 
     print(paramCor_ID)
-    print(paramCor_ID.iloc[0])
-    print(paramCor_ID.iloc[0].BCSm)
+    print(cached)
+    print(cached.BCSm)
     
     Pmax = 2
-    BCSm = paramCor_ID.iloc[0].BCSm
+    BCSm = cached.BCSm
     BCSopt=BCSm-0.5
-    tb1 = paramCor_ID.iloc[0].tb1
-    tb2 = paramCor_ID.iloc[0].tb2
-    tb3 = paramCor_ID.iloc[0].tb3
-    te1 = paramCor_ID.iloc[0].te1
-    te2 = paramCor_ID.iloc[0].te2
-    te3 = paramCor_ID.iloc[0].te3
+    tb1 = cached.tb1
+    tb2 = cached.tb2
+    tb3 = cached.tb3
+    te1 = cached.te1
+    te2 = cached.te2
+    te3 = cached.te3
      
     
     paramCor_ID = paramCor_ID.fillna(0)
-    kb1 = paramCor_ID.iloc[0].kb1
-    kb2 = paramCor_ID.iloc[0].kb2
-    kb3 = paramCor_ID.iloc[0].kb3
-    kp1 = paramCor_ID.iloc[0].kp1
-    kp2 = paramCor_ID.iloc[0].kp2
-    kp3 = paramCor_ID.iloc[0].kp3
+    kb1 = cached.kb1
+    kb2 = cached.kb2
+    kb3 = cached.kb3
+    kp1 = cached.kp1
+    kp2 = cached.kp2
+    kp3 = cached.kp3
     
     argsB = (kb1, kb2, kb3, kp1, kp2, kp3)
     
